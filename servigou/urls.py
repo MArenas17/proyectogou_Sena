06ddccec3abbf0d2d19cf96cc780e62a355d7049
sara-prog17 <sarota1702@gmail.com>
2023-02-27 19:02:30 -0500

    Reg 1

--- servigou/urls.py
+++ servigou/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.index, name= 'index'),
+    
     #URL DE PUBLICACION
     path('CrearPublicacion/', views.crearpublicacion, name='Crearpublicacion' ),
     path('verP/', views.verP, name='verP'),
